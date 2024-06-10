9b54d80f345037ac0ffe51696699796bcd7d56b5
ANTOINE FOUGA <antoine.fouga1@gmail.com>
2024-06-10 13:27:50 +0200

    Js step 4

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -21,10 +21,22 @@ bouttonChange.addEventListener("click", () => {
     const newBackground = prompt("Enter a color");
     const newName = prompt("Enter your name :");
     const nameSelector = document.getElementById("firstname");
-    const background = document.querySelector(".pink-bg");
+
+    const background = document.querySelectorAll(".pink-bg");
+    background.forEach(element => {
+        element.style.background = newBackground;
+    })
+
+
+    const textPink = document.querySelectorAll(".pink-text");
+    textPink.forEach(element => {
+        element.style.color = newBackground;
+    })
+
     nameSelector.textContent = newName;
     nameSelector.style.color = "white";
     background.style.background = newBackground;
+    textPink.style.color = newBackground;
 
 });
 
