667f34e15fa70a0c4bef8feaa68ea9d455c09a77
ANTOINE FOUGA <antoine.fouga1@gmail.com>
2024-06-10 13:02:13 +0200

    javascript step 1, 2 et 3

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,33 @@
-const avatar = document.queryselector(".avatar--bis");
 
-avatar.addEventListener('click', () => {
-    avatar.src = "./image/avatar.svg"
-})
+const avatar = document.getElementById("avatar");
+
+function changeImage() {
+    const img = document.getElementById("avatar");
+    img.src = "./image/avatar.svg";
+}
+avatar.addEventListener("click", changeImage);
+
+
+
+
+
+
+const bouttonChange = document.createElement("button");
+bouttonChange.textContent = "Change name and color";
+
+
+
+bouttonChange.addEventListener("click", () => {
+    const newBackground = prompt("Enter a color");
+    const newName = prompt("Enter your name :");
+    const nameSelector = document.getElementById("firstname");
+    const background = document.querySelector(".pink-bg");
+    nameSelector.textContent = newName;
+    nameSelector.style.color = "white";
+    background.style.background = newBackground;
+
+});
+
+const parent = document.querySelector(".pink-bg");
+parent.appendChild(bouttonChange);
+
