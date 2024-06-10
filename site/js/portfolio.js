ab3fbc7110c585337afb0bb178bb0208cb95faa6
Rossignol Alex <alexnw33910@gmail.com>
2024-06-10 11:39:40 +0200

    finish JavaScript step 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,18 @@
+// STEP 1
+
 const imgAvatar = document.querySelector('#avatarImage');
 
 imgAvatar.addEventListener('click', () => {
     imgAvatar.src = "image/avatar-bis.png";
 });
+
+// STEP 2
+
+const btnChangeText = document.querySelector('#btn-invite');
+const spanFirstName = document.querySelector('#firstname');
+
+btnChangeText.addEventListener('click', () => {
+    const userFirstName = prompt('Your first name.');
+    spanFirstName.textContent = `${userFirstName}`;
+    spanFirstName.style.color = "white";
+})
