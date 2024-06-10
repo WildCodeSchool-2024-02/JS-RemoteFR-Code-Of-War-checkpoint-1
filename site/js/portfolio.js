4f01f0ac9f5fd268ea707dce22d3fc718b1c185b
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-06-10 11:52:54 +0200

    css base et js etape 1

---
+++ site/js/portfolio.js
@@ -0,0 +1,12 @@
+/*Etape 1 changer l'avatar*/
+
+const avatarChange = document.querySelector(".image_avatar");
+
+avatarChange.addEventListener("click", () => {
+    const img = document.querySelector(".image_avatar");
+  
+    img.src = "image/avatar.svg";
+  });
+
+
+  
