33954d75796d726cbc4ae091a83944d7c204a18b
Adiallo123 <aissatoudiallobd@gmail.com>
2024-06-10 14:59:07 +0200

    test:3 js terminer

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,11 +1,11 @@
 const avatar = document.querySelector(".avatar");
 const firstName = document.querySelector("#firstname");
+const descriptionColor = document.querySelector(".description");
 avatar.addEventListener('click', () =>{
     avatar.src = "../site/image/avatar.svg"
 }
     
 )
-
 firstName.textContent = prompt("saisir votre nom");
 firstName.style.color = "white";
-console.log(firstName);
+descriptionColor.style.backgroundColor = "#750ff7";
