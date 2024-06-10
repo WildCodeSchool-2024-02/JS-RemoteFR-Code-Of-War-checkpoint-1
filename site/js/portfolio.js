8eb9644b59d12f9f9e37bd2b8b464cd2768fb268
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-06-10 13:07:29 +0200

    js_base_2_OK

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -9,4 +9,20 @@ avatarChange.addEventListener("click", () => {
   });
 
 
+  /*change nom et couleur*/
+
+  const secondBanner = document.querySelector(".description");
+  const btn = document.createElement("button");
+  btn.classList.add("change_name_color");
+  btn.textContent = "Modify text and color";
+  secondBanner.appendChild(btn);
+
   
+  const changeColorName = () => {
+    const user = prompt("choisi un nom")
+    const newName = document.querySelector("#firstname");
+    newName.textContent = user;
+    newName.style.color = "#ffffff";
+    }
+
+  btn.addEventListener("click", () => changeColorName());
