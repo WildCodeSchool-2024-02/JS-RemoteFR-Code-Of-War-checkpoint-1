8e2023d70b36aef94cfa8035e498fac183c9a89e
Davido91390 <david.kabore@hotmail.fr>
2024-06-10 14:01:39 +0200

    étape 1js

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,4 +1,17 @@
 const avatar = document.querySelector(".avatar");
 avatar.addEventListener("click",() =>{
-    alert("click");
+    avatar.src = "/Users/davido/Desktop/Checkpoint-1/JS-RemoteFR-Code-Of-War-checkpoint-1/site/image/avatar.svg";
+});
+
+const button = document.querySelector("button");
+button.addEventListener("click",() => {
+const firstName = document.querySelector("#firstname");
+const colorUser = document.querySelector(".description pink-bg");
+//alert("click");
+   // alert("omg");
+
+
+    //quand tu clique sur le bouton, tu peux changer l'id firstname et la couleur du background
+    colorUser.innerHTML = "Entrer une couleur";
+    firstName.textContent = "Entrer votre prénom";
 })
