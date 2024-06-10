4226e39078f13643949546880c55f929fdaccd70
dgonc <d.goncalves89000@gmail.com>
2024-06-10 12:46:43 +0200

    finish Js [1 : 4]

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -6,7 +6,9 @@ imgAvatar.addEventListener("click", () => {
 
 const btnUser = document.querySelector('.btnChange');
 const nameUser = document.getElementById('firstname');
-const backColor = document.querySelector('.pink-bg')
+const backColor = document.querySelectorAll('.pink-bg');
+const styleColor = document.querySelectorAll('.pink-text');
+
 
 btnUser.addEventListener("click", () => {
     const nameTxt = prompt("What's your name ?");
@@ -14,7 +16,14 @@ btnUser.addEventListener("click", () => {
 
     nameUser.innerHTML = `${nameTxt}`;
     nameUser.style.color = "white";
+    backColor.forEach(backColor => {
+        backColor.style.backgroundColor = `${color}`;
 
-    backColor.style.backgroundColor = `${color}`;
-    
-} )
+        styleColor.forEach(styleColor => {
+            styleColor.style.color = `${color}`;
+        })
+
+    });
+
+
+})
