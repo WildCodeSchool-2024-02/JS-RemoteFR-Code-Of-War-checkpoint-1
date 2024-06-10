72af89aa7a571fcae56a48e38b39f8a235dd61e8
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-06-10 14:24:17 +0200

    Etape 3 JS terminée

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -6,7 +6,24 @@ clicToChange.addEventListener("click", () => {
     changeAvatar.src = "https://i.notrefamille.com/0x450/smart/2014/03/31/chat1.jpg"
 });
 
-const firstname = document.querySelector("#firstname");
 
-firstname.textContent = "Sarah";
-firstname.style.color = "white";
+// const firstname = document.querySelector("#firstname");
+
+// firstname.textContent = "Sarah";
+// firstname.style.color = "white";
+
+const span = document.querySelector("#firstname");
+
+span.addEventListener("click", function () {
+    const backgroundDescription = document.querySelector(".pink-bg");
+    span.textContent = "Sarah";
+    span.style.color = "white";
+    backgroundDescription.style.backgroundColor = "#750ff7";
+
+});
+
+
+
+
+
+
