b6e367f6493bb127c7f0a12c69c8f09f3855ced3
dijan cetingoz <dijan.cetingoz@icloud.com>
2024-06-10 14:17:04 +0200

    js step 2 finish

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,3 +4,10 @@ avatarImg.addEventListener("click", switchAvatar);
 function switchAvatar(){
     avatarImg.src = "image/avatar.svg"
 }
+
+const firstname = document.querySelector("#firstname");
+
+firstname.textContent = "Dijan";
+firstname.style.color = "white";
+
+console.log(newString);
