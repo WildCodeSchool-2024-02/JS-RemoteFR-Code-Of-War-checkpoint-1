01a6cc05cdc55c135c815895598167b680de7e30
PixelKata <guillaumebasilaire@gmail.com>
2024-06-10 10:30:22 +0200

    finish algo1, algo2, algo3 in part 1

--- algo/3/findSeniors.js
+++ algo/3/findSeniors.js
@@ -26,7 +26,37 @@ const persons = [
 */
 
 function findSeniors(persons) {
-  // Your code here !
+
+  const devsWebSeniors = persons.filter(person =>
+    person.job === 'web dev' && person.experience >= 5
+  );
+
+  const dataAnalystsSeniors = persons.filter(person =>
+    person.job === 'data analyst' && person.experience >= 5
+  );
+
+  return [devsWebSeniors, dataAnalystsSeniors];
 }
+
+const persons = [
+  { name: 'Mary', experience: 2, job: 'web dev' },
+  { name: 'Tony', experience: 6, job: 'data analyst' },
+  { name: 'John', experience: 2, job: 'data analyst' },
+  { name: 'Jane', experience: 6, job: 'web dev' },
+  { name: 'Maggie', experience: 2, job: 'web dev' },
+  { name: 'Leonardo', experience: 2, job: 'data analyst' },
+  { name: 'Carla', experience: 4, job: 'data analyst' },
+  { name: 'Mickael', experience: 7, job: 'web dev' },
+  { name: 'Penelope', experience: 7, job: 'web dev' },
+  { name: 'Homer', experience: 5, job: 'data analyst' },
+  { name: 'Leonardo', experience: 2, job: 'data analyst' },
+  { name: 'Carla', experience: 4, job: 'web dev' },
+  { name: 'Lisa', experience: 3, job: 'web dev' },
+  { name: 'Millie', experience: 5, job: 'data analyst' },
+  { name: 'Penelope', experience: 7, job: 'web dev' },
+];
+
+console.log(findSeniors(persons));
+
 
 module.exports = findSeniors;
