bf02c9e4a4afbfae4ebca48af1c3f14ff66b941b
dijan cetingoz <dijan.cetingoz@icloud.com>
2024-06-10 14:31:58 +0200

    algo 3 finish

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -10,4 +10,8 @@ const firstname = document.querySelector("#firstname");
 firstname.textContent = "Dijan";
 firstname.style.color = "white";
 
+document.body.style.backgroundColor = "pink";
+console.log(document.body.style.backgroundColor);
+document.body.style.backgroundColor = "#750ff7";
+
 console.log(newString);
